a468eeb59e5100602127338e11482534ef699cb0
Juliana <julianasi@mail.ru>
2021-12-05 03:13:01 +0300

    Решение задач 1 и 3 не изменилось, но написала мысли о другом варианте решения, которое не знаю как правильно осуществить. Остальные задачи самостоятельно решить не смогла, т.к. увидела вариант решения на третьем уроке.

---
+++ hw_28.11.21/hw_4.py
@@ -0,0 +1,6 @@
+user_input = input('Sentence: ')
+words = user_input.split()
+for num, word in enumerate(words):
+    print(f'#{num+1} - {word[:10]}')
+
+# Это решение скопировала у Вас, чтобы попробовать написать код.
